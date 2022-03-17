64de91e5ab7f8490ab6eab2e75af20b64adc81c4
lucy-butler <chaebonggi@naver.com>
2022-03-17 18:25:49 +0900

    백준 문제풀이 2753

---
+++ B2753/app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+input = input[0];
+solution(+input);
+function solution(num) {
+
+    if((num % 4 === 0 && num % 100 !==0) || num % 400 === 0){
+        console.log(1);
+        return;
+    }
+    console.log(0);
+    
+}
